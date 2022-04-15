b8dc2f19d8af2973fdf380615883be6b3c1194c0
josechaparro <josechaparro.dev@gmail.com>
2022-04-14 23:05:05 -0700

    first markup complete for mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ['./dist/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: 'hsl(257, 40%, 49%)',
+        magenta: 'hsl(300, 69%, 71%)',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        openSans: ['Open Sans', 'sans-serif'],
+      },
+      boxShadow: {
+        '3xl': '2px 2px 3px 3px rgba(0, 0, 0, 0.3)',
+      },
+    },
   },
   plugins: [],
 };
